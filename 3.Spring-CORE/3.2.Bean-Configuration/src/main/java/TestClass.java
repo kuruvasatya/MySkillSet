fa44b3289ef6f8b0288bde6227d1a0cf8082a32f
Satya <satyaganesh987@gmail.com>
2023-10-30 16:39:43 +0530

    3.2 added more comments

--- 3.Spring-CORE/3.2.Bean-Configuration/src/main/java/TestClass.java
+++ 3.Spring-CORE/3.2.Bean-Configuration/src/main/java/TestClass.java
@@ -3,6 +3,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestClass {
     public static void main(String args[]) {
+        /**
+         * Application context uses eager loading, so all the beans are createad
+         * as soon as application context is created.
+         *
+         * where are if we are using bean factory, beans are createad when we call getBean() method
+         */
         ApplicationContext context = new ClassPathXmlApplicationContext("vehicle.xml");
         Bus b = (Bus) context.getBean("bus");
         Train t = (Train) context.getBean("train");
