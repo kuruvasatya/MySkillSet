b82cde49159ad1e63905c492f0125971ccfafb44
Satya <satyaganesh987@gmail.com>
2023-10-30 16:52:21 +0530

    3.2 added how to lazy init few beans

--- 3.Spring-CORE/3.2.Bean-Configuration/src/main/java/TestClass.java
+++ 3.Spring-CORE/3.2.Bean-Configuration/src/main/java/TestClass.java
@@ -4,10 +4,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestClass {
     public static void main(String args[]) {
         /**
-         * Application context uses eager loading, so all the beans are createad
+         * Application context uses eager loading, so all the beans are created
          * as soon as application context is created.
          *
-         * where are if we are using bean factory, beans are createad when we call getBean() method
+         * whereas if we are using bean factory, beans are created when we call getBean() method
          */
         ApplicationContext context = new ClassPathXmlApplicationContext("vehicle.xml");
         Bus b = (Bus) context.getBean("bus");
