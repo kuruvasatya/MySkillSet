33024d6782cb3edb6bd4f6e08b53ef0601443037
Satya <satyaganesh987@gmail.com>
2023-11-14 14:49:51 +0530

    clean up

--- 4.Spring-MVC/3.SpringMVC-JavaBased/src/main/java/com/web/ApplicationConfiguration.java
+++ 4.Spring-MVC/3.SpringMVC-JavaBased/src/main/java/com/web/ApplicationConfiguration.java
@@ -3,13 +3,16 @@ package com.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages="com.web")
-public class ApplicationConfiguration {
+public class ApplicationConfiguration implements WebMvcConfigurer {
 	
 	@Bean
+	@GetMapping
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/views/");
