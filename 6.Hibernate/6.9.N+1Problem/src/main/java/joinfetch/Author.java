8dc99a18a8be91f141dd9275f8e9af112ca353cc
Satya <satyaganesh987@gmail.com>
2023-12-09 19:11:52 +0530

    6.9 added join fetch

---
+++ 6.Hibernate/6.9.N+1Problem/src/main/java/joinfetch/Author.java
@@ -0,0 +1,54 @@
+package joinfetch;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    Address address;
+
+    public Author() {
+    }
+
+    public Author(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
---
+++ 6.Hibernate/6.9.N+1Problem/src/main/java/joinfetch/Book.java
@@ -0,0 +1,56 @@
+package joinfetch;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    Author author;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    Customer customer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
