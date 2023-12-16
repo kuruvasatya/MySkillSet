8c8c9ced4cfa8f884beb44c7987ee399a63622f4
Satya <satyaganesh987@gmail.com>
2023-12-16 21:24:31 +0530

    7. Explore more about @EnableAutoConfiguration Annotation

--- 7.SpringBootFoundation/7.1.Hello-World/src/main/java/com/learning/HelloWorld/Application.java
+++ 7.SpringBootFoundation/7.1.Hello-World/src/main/java/com/learning/HelloWorld/Application.java
@@ -1,13 +1,34 @@
 package com.learning.HelloWorld;
 
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		/**
+		 * 1. SpringApplication without any source
+		 * error: Application run failed: Sources must not be empty
+		 */
+//		SpringApplication springApplication = new SpringApplication();
+//		springApplication.run();
+
+		/**
+		 * 2. SpringApplication with source and without @EnableAutoConfiguration annotation
+		 * error: Exception encountered during context initialization - cancelling refresh attempt: org.springframework.context.ApplicationContextException: Unable to start web server
+		 */
+//		SpringApplication springApplication = new SpringApplication(Application.class);
+//		springApplication.run();
+
+		/**
+		 * 3. SpringApplication with source and with @EnableAutoConfiguration annotation but no @ComponentScan annotation
+		 * error: REST API return 404 not found,
+		 */
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.run();
 	}
 
 }
